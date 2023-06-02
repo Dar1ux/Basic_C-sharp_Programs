9a724cc74094fc60c04c17964d93429b06d58a8c
Dar1ux <djankaus87@gmail.com>
2023-06-02 12:13:13 +0300

    Math Challenge

---
+++ Math Challenge/Math Challenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math_Challenge
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = 20;
+            int num2 = 5;
+            int result;
+            result = num1 + num2;
+            Console.WriteLine(num1.ToString() + " + " + num2.ToString() + " = " + result.ToString());
+            result = num1 - num2;
+            Console.WriteLine(num1.ToString() + " - " + num2.ToString() + " = " + result.ToString());
+            result = num1 * num2;
+            Console.WriteLine(num1.ToString() + " * " + num2.ToString() + " = " + result.ToString());
+            result = num1 / num2;
+            Console.WriteLine(num1.ToString() + " / " + num2.ToString() + " = " + result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
