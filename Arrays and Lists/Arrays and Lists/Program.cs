2035d27c549f62f6e1bd7e3a364ffac914c81e4f
Dar1ux <djankaus87@gmail.com>
2023-06-12 14:10:44 +0300

    Tutorial exercises

---
+++ Arrays and Lists/Arrays and Lists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> intList = new List<string>();
+            intList.Add("Hello!");
+            intList.Add("Jesse");
+            intList.Remove("Jesse");
+            Console.WriteLine(intList[0]);
+
+
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+            //int[] numArray1 = new int[] { 5, 2, 100, 200, 5000 };
+
+            //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+            //Console.WriteLine(numArray2[3]);
+            Console.ReadLine();
+        }
+    }
+}
