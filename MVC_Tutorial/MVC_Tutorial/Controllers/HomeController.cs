d38d96574ad35bfd32ce45af6f3a3abc6daeb52a
Dar1ux <djankaus87@gmail.com>
2023-06-26 10:50:25 +0300

    MVC Tutorial

---
+++ MVC_Tutorial/MVC_Tutorial/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using MVC_Tutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Tutorial.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"C:\Users\djank\Documents\Pitman Training\Basic_C-sharp_Programs\MVC_Tutorial\log.txt", text);
+
+            //Random rnd = new Random();
+            //int num = rnd.Next();
+
+            ////ViewBag.RandomNumber = num;
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //return RedirectToAction("Contact");
+
+            //List<string> names = new List<string>
+            //{
+            //    "Jesse",
+            //    "Adam",
+            //    "Brett"
+            //};
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Jesse";
+            user.LastName = "Johnson";
+            user.Age = 23;
+
+            return View(user);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(int id=0)
+        {
+            ViewBag.Message = id;
+
+            return View();
+        }
+    }
+}
